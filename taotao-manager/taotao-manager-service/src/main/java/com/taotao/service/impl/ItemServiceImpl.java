1821cd3112a95b672f27834cfa28ce01e136d25e
minGW <397546139@qq.com>
2018-04-20 07:40:54 +0800

    content管理

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -1,15 +1,20 @@
 package com.taotao.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
-import com.taotao.mapper.TbItemMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.IDUtils;
+import com.taotao.mapper.TbItemDescMapper;
+import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemDesc;
 import com.taotao.pojo.TbItemExample;
 import com.taotao.service.ItemService;
 
@@ -17,7 +22,7 @@ import com.taotao.service.ItemService;
  * 商品管理Service
  * <p>Title: ItemServiceImpl</p>
  * <p>Description: </p>
- * <p>Company: www.itcast.cn</p> 
+ * <p>Company: www.itcast.cn</p>
  * @version 1.0
  */
 @Service
@@ -25,7 +30,9 @@ public class ItemServiceImpl implements ItemService {
 
 	@Autowired
 	private TbItemMapper itemMapper;
-	
+	@Autowired
+	private TbItemDescMapper itemDescMapper;
+
 	@Override
 	public TbItem getItemById(long itemId) {
 		TbItem item = itemMapper.selectByPrimaryKey(itemId);
@@ -46,6 +53,31 @@ public class ItemServiceImpl implements ItemService {
 		result.setTotal(pageInfo.getTotal());
 		//返回结果
 		return result;
+	}
+
+	@Override
+	public TaotaoResult addItem(TbItem item, String desc) {
+		//生成商品id
+		long itemId = IDUtils.genItemId();
+		//补全item的属性
+		item.setId(itemId);
+		//商品状态，1-正常，2-下架，3-删除
+		item.setStatus((byte) 1);
+		item.setCreated(new Date());
+		item.setUpdated(new Date());
+		//向商品表插入数据
+		itemMapper.insert(item);
+		//创建一个商品描述表对应的pojo
+		TbItemDesc itemDesc = new TbItemDesc();
+		//补全pojo的属性
+		itemDesc.setItemId(itemId);
+		itemDesc.setItemDesc(desc);
+		itemDesc.setUpdated(new Date());
+		itemDesc.setCreated(new Date());
+		//向商品描述表插入数据
+		itemDescMapper.insert(itemDesc);
+		//返回结果
+		return TaotaoResult.ok();
 	}
 
 }
